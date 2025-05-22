69e821cf2399050006fe09c6c298ed228fcfe298
Pavel Zubko <tyyrok@gmail.com>
2025-05-22 16:39:03 +0400

    add simple file structure parser

---
+++ main.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	"baliance.com/gooxml/document"
+)
+
+type TreeStorage struct {
+	Value map[string]any
+	mutex sync.Mutex
+}
+
+func (t *TreeStorage) addElem(key string, value any) {
+	t.mutex.Lock()
+	t.Value[key] = value
+	t.mutex.Unlock()
+}
+
+
+func main() {
+	/*
+	var flag string
+	if len(os.Args) > 0 {
+		flag = os.Args[1]
+	} else {
+		flag = ""
+	}
+	fmt.Println(flag)
+	*/
+	var wg sync.WaitGroup
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Current directory:", dir)
+	storage := &TreeStorage{Value: map[string]any{}, mutex: sync.Mutex{}}
+
+	files, _ := os.ReadDir(dir)
+	for _, file := range files {
+		fpath := filepath.Join(dir, file.Name())
+		if strings.HasPrefix(file.Name(), ".") || file.Name() == "structure.docx" {
+			continue
+		}
+		if file.IsDir() {
+			wg.Add(1)
+			go recursiveFileParser(fpath, &wg, storage)
+		} else {
+			b, err := os.ReadFile(fpath)
+			if err == nil {
+				file_text := string(b)
+				storage.addElem(file.Name(), file_text)
+			}
+		}
+	}
+	wg.Wait()
+	printTreeStructure("", storage.Value)
+	saveStructureToDocx(&storage.Value)
+}
+
+func recursiveFileParser(dir string, wg *sync.WaitGroup, storage *TreeStorage) {
+	files, _ := os.ReadDir(dir)
+	res := map[string]any{}
+	for _, file := range files {
+		if strings.HasPrefix(file.Name(), ".") {
+			continue
+		}
+		fpath := filepath.Join(dir, file.Name())
+		if file.IsDir() {
+			res := recursiveSyncFileParser(fpath)
+			res[file.Name()] = res
+			continue
+		}
+		b, err := os.ReadFile(fpath)
+		if err != nil {
+			continue
+		}
+		file_text := string(b)
+		res[file.Name()] = file_text
+	}
+	storage.addElem(filepath.Base(dir), res)
+	wg.Done()
+}
+
+func recursiveSyncFileParser(dir string) map[string]any {
+	files, _ := os.ReadDir(dir)
+	res := map[string]any{}
+	for _, file := range files {
+		fpath := filepath.Join(dir, file.Name())
+		if file.IsDir() {
+			res[file.Name()] = recursiveSyncFileParser(fpath)
+			continue
+		}
+		b, err := os.ReadFile(fpath)
+		if err != nil {
+			continue
+		}
+		file_text := string(b)
+		res[file.Name()] = file_text
+	}
+	return res
+}
+
+func printTreeStructure(prefix string, tree map[string]any) {
+	p := prefix + "|--"
+	
+	for k, v := range tree {
+		fmt.Printf("%s%s\n", p, k)
+		value, ok := v.(map[string]any)
+		if ok {
+			printTreeStructure(p, value)
+		}
+	}
+}
+
+func saveStructureToDocx(tree *map[string]any) {
+	doc := document.New()
+	para := doc.AddParagraph()
+	run := para.AddRun()
+	para.SetStyle("Title")
+	run.AddText("Files Tree Structure")
+	addTreeStructure("", tree, doc)
+	para = doc.AddParagraph()
+	para.SetStyle("Title")
+	run.AddText("Files Content")
+	addFilesContent("", tree, doc)
+	doc.SaveToFile("structure.docx")
+}
+
+func addTreeStructure(prefix string, tree *map[string]any, d *document.Document) {
+	p := prefix + "|--"
+	
+	for k, v := range *tree {
+		s := fmt.Sprintf("%s%s\n", p, k)
+		para := d.AddParagraph()
+		run := para.AddRun()
+		run.AddText(s)
+		value, ok := v.(map[string]any)
+		if ok {
+			addTreeStructure(p, &value, d)
+		}
+	}
+}
+
+func addFilesContent(name_prefix string, tree *map[string]any, d *document.Document) {
+	for k, v := range *tree {
+		var title string
+		if name_prefix != "" {
+			title = fmt.Sprintf("%s/%s\n", name_prefix, k)
+		} else {
+			title = fmt.Sprintf("%s\n", k)
+		}
+		para := d.AddParagraph()
+		para.Properties().SetHeadingLevel(2)
+		para.AddRun().AddText(title)
+		value, ok := v.(map[string]any)
+		if ok {
+			addFilesContent(title, &value, d)
+			continue
+		}
+		lines := strings.Split(v.(string), "\n")
+		for _, line := range lines {
+			para := d.AddParagraph()
+			run := para.AddRun()
+			run.AddText(line)
+			run.Properties().SetFontFamily("Courier New")
+			run.Properties().SetSize(9)
+		}
+	}
+}
